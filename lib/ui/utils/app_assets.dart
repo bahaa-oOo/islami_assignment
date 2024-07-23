09b9fbaa75aaf66753e87890ada7657db9c514b8
USER <bahaamajdi963@gmail.com>
2024-07-24 00:09:01 +0300

    Add base project

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,11 @@
+abstract class AppAssets{
+  static const String defaultPath = "assets/images/";
+  static const String splash = "${defaultPath}splash.png";
+  static const String background = "${defaultPath}default_bg.png";
+  static const String icQuran = "${defaultPath}icon_quran.png";
+  static const String icAhdath = "${defaultPath}icon_hadeth.png";
+  static const String icSebha = "${defaultPath}body_sebha_logo.png";
+  static const String icRadio = "${defaultPath}icon_radio.png";
+  static const String quranTapLogo = "${defaultPath}qur2an_screen_logo.png";
+  static const String ahadethTapLogo = "${defaultPath}hadeth_logo.png";
+}
---
+++ lib/ui/utils/app_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:islami/ui/utils/app_colors.dart';
+
+abstract class AppStyle {
+ static const TextStyle appBarTextStyle = TextStyle(color: AppColors.accent , fontSize: 30 ,fontWeight:FontWeight.bold );
+ static const TextStyle Titles = TextStyle(color: AppColors.accent , fontSize: 25 ,fontWeight:FontWeight.w600 );
+
+}
